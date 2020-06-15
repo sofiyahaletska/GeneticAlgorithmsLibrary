148904f1b85c00a9a754a5992c8c73400513784e
Julia Pochynok <47351044+juliapochynok@users.noreply.github.com>
2020-06-15 13:12:26 +0300

    Add documentation

--- get_mutation.cpp
+++ get_mutation.cpp
@@ -2,6 +2,13 @@
 #include "genetic.h"
 
 int* Genetic::resetMutation(int* individual){
+    /**
+   * @brief This function perform mutation of gens by replacing certain amount
+   * of individual's gens (relying on mutation_rate) by random generated gen
+   *
+   * @param individual
+   * Gens of the individual that is going to mutate
+    */
     for (int x = 0; x < mutation_rate; x++) {
         individual[x] = getRand(minim, maxim);
     }
@@ -9,6 +16,14 @@ int* Genetic::resetMutation(int* individual){
 }
 
 int* Genetic::gaussMutation(int *individual) {
+    /**
+    * @brief This function perform mutation in which the gene that
+    * goes through mutation is replaced with a number that is generated
+    * according to gauss distribution around the original gene.
+    *
+    * @param individual
+    * Gens of the individual that is going to mutate
+    */
     for (int x = 0; x < mutation_rate; x++) {
         int mean;
 
@@ -41,6 +56,14 @@ int* Genetic::gaussMutation(int *individual) {
 
 // mutation for simpler genoms with small amount of gens
 int* Genetic::swapMutation(int* individual) {
+    /**
+    * @brief This function perform mutation of gens by swapping gens at
+    * extreme ends of gens sequence
+    * IMPORTANT! This mutation is for the sequences with big amount of gens
+    *
+    * @param individual
+    * Gens of the individual that is going to mutate
+    */
     if (n_variables != 1) {
         int a = individual[0] ;
         int b = individual[n_variables-1];
@@ -52,6 +75,14 @@ int* Genetic::swapMutation(int* individual) {
 
 // mutation for complicated genoms with more than 3 gens
 int* Genetic::inversionMutation(int* individual) {
+    /**
+    * @brief This function perform mutation of gens by inverting
+    * the middle part of the gens sequence
+    * IMPORTANT! This mutation is for the sequences with big amount of gens
+    *
+    * @param individual
+    * Gens of the individual that is going to mutate
+    */
     if (n_variables > 3) {
         int middle = n_variables/2;
         for (int i = 1; i < middle; i++) {
